0c12c1da3f1d1b1f04f6a9f56178ce09962fae11
Schismond <dadourachid720@gmail.com>
2023-05-26 18:13:03 +0100

    fix background image name + position use html semantic elements add text outline for better visibility fix JS

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-//variables 
+let choiceElement = document.getElementById("choice");
+let checkButton = document.getElementById("check");
+let resultElement = document.getElementById("result");
 
-let myChoice = document.getElementById('choice');
-let myButton = document.getElementById('putAclick');
-let myResult = document.getElementById('result');
-let pcNumber = Math.floor(Math.random() * 100 + 1);
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-//event listner
-myButton.addEventListener('click', function(){
-    let playerNumber = `${myChoice.value}`;
-    console.log(playerNumber);
-    console.log(pcNumber);
-        if(playerNumber==pcNumber){
-             myResult.innerText= ("🥳We Have a WINNER🥳");
-             myButton.innerText = ('play again');
-             myButton.addEventListener('click', function(){
-                document.location.reload(false);
-             });
-        }
-        else if (playerNumber > pcNumber){
-            myResult.innerText=("😢you are higher😢");
-            if (playerNumber - pcNumber >10){
-                myResult.innerText=("😭you are way too high😭");
-            }
-        }
-        else {
-            myResult.innerText=("😢 you are lower 😢");
-            if (pcNumber - playerNumber >10){
-                myResult.innerText=("😭you are way too low😭");
-            }
-        }
-});
+let pcNumber = getRandomNumber(1, 100);
+
+checkButton.addEventListener("click", handleButtonClick);
+
+function handleButtonClick() {
+  let playerNumber = `${choiceElement.value}`;
+
+  if (playerNumber === pcNumber.toString()) {
+    let winnerMessage = "🥳 We have a WINNER 🥳";
+    displayResult(winnerMessage);
+    checkButton.innerText = "Play Again";
+    checkButton.addEventListener("click", function () {
+      document.location.reload(false);
+    });
+  } else if (playerNumber > pcNumber) {
+    let highMessage = "😢 You are higher 😢";
+    if (playerNumber - pcNumber > 10) {
+      highMessage = "😭 You are way too high 😭";
+    }
+    displayResult(highMessage);
+  } else {
+    let lowMessage = "😢 You are lower 😢";
+    if (pcNumber - playerNumber > 10) {
+      lowMessage = "😭 You are way too low 😭";
+    }
+    displayResult(lowMessage);
+  }
+}
+
+// Function to display the result
+function displayResult(message) {
+  resultElement.innerText = message;
+}
